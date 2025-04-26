0f867c460594255637b5c15d54d330091ebde84a
Alex Kigo Mutitu <alexmutitu@strathmore.edu>
2025-04-26 17:07:45 +0300

    commit for qsn1

---
+++ qsn1.py
@@ -0,0 +1,15 @@
+#1. Sum all elements in a list.
+#Adding numbers in a list
+list_num=[100,500,600,300]
+sum=0
+for num in list_num:
+    sum+=num
+print(sum)
+
+#adding strings in a list
+list_str=["Hello","I","am","Alex"]
+sum=""
+for str in list_str:
+    sum+=str
+print(sum)
+
